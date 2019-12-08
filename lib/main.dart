9068f551595b889a95d813484ff8949281e0ad65
lenovo <hadil.alsaffar@gmail.com>
2019-12-08 15:28:33 +0300

    added comment 323498789

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
 
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blue, // hi girls iam testing
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
