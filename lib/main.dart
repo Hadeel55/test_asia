396e2e6c3f33135c109acdaacaab7710bb9d0876
lenovo <hadil.alsaffar@gmail.com>
2019-12-08 15:00:04 +0300

    added comment

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {//i didinot do any change add me please
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
